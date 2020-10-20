d9cb5dcf19235f234a58ad02b165781bc042657f
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-20 16:32:08 +0700

    Initial commit

---
+++ lib/App/FilenameUtils.pm
@@ -0,0 +1,23 @@
+package App::FilenameUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: CLIs for Filename::*
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+This distribution includes several utilities related to Filename::* modules:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Filename::Archive>, L<Filename::Audio>, etc.
